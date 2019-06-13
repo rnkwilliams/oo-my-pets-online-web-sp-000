a6c5f4f86fcd07c6ee87d52b52c468a30785d67c
Ranika Williams <rnkprice07@gmail.com>
2019-06-13 17:41:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -13,8 +13,25 @@ class Owner
   end
 
   def say_species
-
+    return "I am a #{species}."
   end
+
+  #Pets
+
+  def buy_fish(name_of_fish)
+    @pets[:fishes] << Fish.new(name_of_fish)
+  end
+
+  def buy_dog(name_of_dog)
+    @pets[:dogs] << Dog.new(name_of_dog)
+  end
+
+  def buy_cat(name_of_cat)
+    @pets[:cats] << Cat.new(name_of_cat)
+  end
+
+  
+  
 
 
 

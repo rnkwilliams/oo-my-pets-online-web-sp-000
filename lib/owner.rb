a66b6a646186e6c07c77a6a16cdca1ce99f051bd
Ranika Williams <rnkprice07@gmail.com>
2019-06-13 17:28:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,8 +2,16 @@ class Owner
   attr_accessor :pets
   attr_reader :species
 
+  @@all = []
+
   def initialize(species = nil)
     @species = species
     @pets = {}
   end
+
+  def self.all
+    @@all
+  end
+
+  
 end
